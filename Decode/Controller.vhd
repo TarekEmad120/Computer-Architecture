556ef24df13f3217beae057d7be98e289adce9b9
ahmedwaleed01 <ahmed.waleed1303@gmail.com>
2024-04-19 16:52:46 +0200

    refactory controller code

--- Decode/Controller.vhd
+++ Decode/Controller.vhd
@@ -23,30 +23,55 @@ end entity;
 architecture IMP of Controller is
 
 begin
-  aluControl    <= Func & one_two_attrib when oppCode = "001";
-  WRB_S         <= "10" when oppCode = "001"; -- select ALU Data to write Back
-  WRITE_BACK    <= '1' when oppCode = "001";
-  Free_P_Enable <= '0' when oppCode = "001";
+  PROCESS (oppCode) IS
+  BEGIN
+  case oppCode is
+    when "001" =>
+      aluControl    <= Func & one_two_attrib;
+      WRB_S         <= "10";
+      WRITE_BACK    <= '1';
+      Free_P_Enable <= '0';
+      RA2_SEL       <= "00";
+      RS2_RD_SEL    <= '0';
+      MEM_READ      <= '0';
+      MEM_WRITE     <= '0';
+      Mem_protect_enable <= '0';
+      Mem_free_enable    <= '0';
+      case one_two_attrib is 
+        when  '0' =>
+          RS1_RD_SEL <= '1';
+        when others => RS1_RD_SEL <= '0';
+      end case;
+      when others=>
+  end case;
+  end process;
 
-  -- to be edited --
-RA2_SEL <= "00" when oppCode = "001" else
-    "01";
 
-  -- to be edited --
-  RS2_RD_SEL <= '0' when oppCode = "001";
-  RS1_RD_SEL <= '1' when one_two_attrib = '0' else
-                 '0';
 
-  MEM_READ           <= '0' when oppCode = "001";
-  MEM_WRITE          <= '0' when oppCode = "001";
-  Mem_protect_enable <= '0' when oppCode = "001";
-  Mem_free_enable    <= '0' when oppCode = "001";
-  -- LDM OPERATION 
-  WRITE_BACK         <= '1' when oppCode = "100" and Func = "011";
-  WRB_S              <= "11" when oppCode = "100" and Func = "011";
-  MEM_READ           <= '0' when oppCode = "100" and Func = "011";
-  MEM_WRITE          <= '0' when oppCode = "100" and Func = "011";
-  Mem_protect_enable <= '0' when oppCode = "100" and Func = "011";
-  Mem_free_enable    <= '0' when oppCode = "100" and Func = "011";
+--   aluControl    <= Func & one_two_attrib when oppCode = "001";
+--   WRB_S         <= "10" when oppCode = "001"; -- select ALU Data to write Back
+--   WRITE_BACK    <= '1' when oppCode = "001";
+--   Free_P_Enable <= '0' when oppCode = "001";
+
+--   -- to be edited --
+-- RA2_SEL <= "00" when oppCode = "001" else
+--     "01";
+
+--   -- to be edited --
+--   RS2_RD_SEL <= '0' when oppCode = "001";
+--   RS1_RD_SEL <= '1' when one_two_attrib = '0' else
+--                  '0';
+
+--   MEM_READ           <= '0' when oppCode = "001";
+--   MEM_WRITE          <= '0' when oppCode = "001";
+--   Mem_protect_enable <= '0' when oppCode = "001";
+--   Mem_free_enable    <= '0' when oppCode = "001";
+--   -- LDM OPERATION 
+--   WRITE_BACK         <= '1' when oppCode = "100" and Func = "011";
+--   WRB_S              <= "11" when oppCode = "100" and Func = "011";
+--   MEM_READ           <= '0' when oppCode = "100" and Func = "011";
+--   MEM_WRITE          <= '0' when oppCode = "100" and Func = "011";
+--   Mem_protect_enable <= '0' when oppCode = "100" and Func = "011";
+--   Mem_free_enable    <= '0' when oppCode = "100" and Func = "011";
 
 end architecture;
