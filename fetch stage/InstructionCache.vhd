a69b140f75b7c4928891c89aac4da5dc0351b17f
Tarek Emad <tarek.mohamed04@eng-st.cu.edu.eg>
2024-04-19 16:33:30 +0200

    instructuions txt

--- fetch stage/InstructionCache.vhd
+++ fetch stage/InstructionCache.vhd
@@ -1,52 +1,53 @@
-LIBRARY IEEE;
-USE ieee.std_logic_1164.ALL;
-USE ieee.std_logic_textio.ALL;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
-USE IEEE.numeric_std.ALL;
-USE std.textio.ALL;
+library IEEE;
+  use ieee.std_logic_1164.all;
+  use ieee.std_logic_textio.all;
+  use IEEE.STD_LOGIC_UNSIGNED.all;
+  use IEEE.numeric_std.all;
+  use std.textio.all;
 
+  -- the memory modulenis 4k of 16 bit words with 16 bit address
 
-
--- the memory modulenis 4k of 16 bit words with 16 bit address
 entity InstructionMemory is
-    Port ( clk : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           address : in  STD_LOGIC_VECTOR (11 downto 0);
-           data : out  STD_LOGIC_VECTOR (15 downto 0));
-end InstructionMemory;
+  port (clk     : in  STD_LOGIC;
+        reset   : in  STD_LOGIC;
+        address : in  STD_LOGIC_VECTOR(11 downto 0);
+        data    : out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
 
 architecture Behavioral of InstructionMemory is
 
-type memory is array (0 to 4095) of std_logic_vector(15 downto 0);
-signal ram : memory;
+    TYPE memory_array IS ARRAY (0 TO 4095) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL ram : memory_array;
+    SIGNAL initial_flag : STD_LOGIC := '1';
+BEGIN
 
-begin
+    instruction_memory : PROCESS (clk,reset,address) IS
+        FILE memory_file : text;
+        VARIABLE file_line : line;
+        VARIABLE temp_data : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    BEGIN
+        IF (RESET = '1') THEN
+            -- RAM <= (OTHERS => (OTHERS => '0'));
+            data <= (OTHERS => '0');
+            INITIAL_FLAG <= '1';
+        ELSIF (initial_flag = '1') THEN
+            file_open(memory_file, "Instructions.txt",  read_mode);
+            FOR i IN 0 TO 4095 LOOP
+                IF NOT endfile(memory_file) THEN
+                    readline(memory_file, file_line);
+                    read(file_line, temp_data);
+                    ram(i) <= temp_data;
+                -- ELSE
+                --     file_close(memory_file);
 
-process(clk, reset)
-    file file_handle : text open read_mode is "F:\Computer Archectiture\project\Computer-Architecture\Instructions.txt";
-    variable line : line;
-    variable value : std_logic_vector(15 downto 0);
-    variable address : integer;
-    variable i : integer;
-    begin
-        if reset = '1' then
-            for i in 0 to 4095 loop
-                ram(i) <= (others => '0');
-            end loop;
-        elsif rising_edge(clk) then
-            if endfile(file_handle) then
-                file_close(file_handle);
-                file_open(file_handle, "C:/Users/Owner/Desktop/Computer Architecture Project/Instructions.txt");
-            end if;
-            readline(file_handle, line);
-            read(line, value);
-            read(line, address);
-            ram(address) <= value;
-        end if;
-    end process;
+                END IF;
+            END LOOP;
+            initial_flag <= '0';
 
+        ELSE  
+            data <= ram(to_integer(unsigned(address)));
+        END IF;
 
-    data <= ram(conv_integer(address));
-
-end Behavioral;
+    END PROCESS instruction_memory;
+END ARCHITECTURE;
 
